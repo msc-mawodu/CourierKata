6129d331800556c0a0b81c9328bb81b0d72fddd0
MaWoDu <durkiewiczmaciej@gmail.com>
2020-06-07 19:12:24 +0100

    init

---
+++ CourierKata/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CourierKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             Generally speaking my "first language" of choice would be JAVA, as I've have most professional experience with it.
+             But considering the fact I'm applying for a position in which C# is being favoured, I'll opt-in for that language.
+            */ 
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
